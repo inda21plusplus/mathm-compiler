eaacc4f7476d1f7ffcf35e0de8c0d0ef9f5aa9d3
mathiasmagnusson <mathiasmagnussons@gmail.com>
2021-10-18 21:31:47 +0200

    Fix unicode bug

--- subset/src/parser/mod.rs
+++ subset/src/parser/mod.rs
@@ -94,6 +94,7 @@ impl fmt::Display for Span {
 
 #[derive(Debug, Clone, Copy)]
 pub struct Input<'s> {
+    /// the location of the input, in bytes
     pub location: usize,
     pub s: &'s str,
 }
@@ -106,11 +107,19 @@ impl<'s> Input<'s> {
         self.s.chars().next().map(|c| {
             (
                 Self {
-                    location: self.location + 1,
-                    s: &self.s[1..],
+                    location: self.location + c.len_utf8(),
+                    s: &self.s[c.len_utf8()..],
                 },
                 c,
             )
         })
+    }
+}
+
+impl<'s> std::ops::Index<Span> for Input<'s> {
+    type Output = str;
+
+    fn index(&self, i: Span) -> &Self::Output {
+        &self.s[i.0.start - self.location..i.0.end - self.location]
     }
 }
